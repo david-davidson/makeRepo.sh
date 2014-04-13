6c2947bc88d0a35d9a15d5d448cb43a32a3b180f
david-davidson <david.davidson@logos.com>
2014-04-12 17:22:06 -0700

    Updated to prompt the user for GitHub username

--- mkGitDir.sh
+++ mkGitDir.sh
@@ -2,4 +2,5 @@
 
 # 1. Set up an alias by editing .bash_profile: e.g., mkGitDir='path/to/this/script/mkGitDir.sh'
 # 2. Then just run mkGitDir from anywhere, and name your repo after: e.g., mkGitDir test
-curl -u "david-davidson" https://api.github.com/user/repos -d '{"name":"'$1'"}'
+echo "What's your GitHub username?" ; read USERNAME
+curl -u "$USERNAME" https://api.github.com/user/repos -d '{"name":"'$1'"}'
