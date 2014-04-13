3eea38d6b59ab5b9c80a78b82cb7c1f0e1f81e19
david-davidson <david.davidson@logos.com>
2014-04-12 17:01:05 -0700

    Trimmed it to just one line

--- mkGitDir.sh
+++ mkGitDir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Set up an alias by editing .bash_profile: e.g., mkGitDir='path/to/this/script/mkGitDir.sh'
-# Then just run mkGitDir from anywhere!
-echo "Name ur new repo:" ; read REPO_NAME
-curl -u "david-davidson" https://api.github.com/user/repos -d '{"name":"'$REPO_NAME'"}'
+
+# 1. Set up an alias by editing .bash_profile: e.g., mkGitDir='path/to/this/script/mkGitDir.sh'
+# 2. Then just run mkGitDir from anywhere, and name your repo after: e.g., mkGitDir test
+curl -u "david-davidson" https://api.github.com/user/repos -d '{"name":"'$1'"}'
